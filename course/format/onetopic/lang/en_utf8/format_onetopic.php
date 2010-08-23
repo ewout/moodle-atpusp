5f222c8440c440b2179e34950894f92fe0e70160
Ewout ter Haar <ewout@usp.br>
2010-08-23 09:16:03 -0300

    Novo formato de curso "onetopic" (abas)

---
+++ course/format/onetopic/lang/en_utf8/format_onetopic.php
@@ -0,0 +1,4 @@
+<?php
+$string['formatonetopic'] = 'Topics in tabs'; // Name to display for format
+$string['nameonetopic'] = 'topic'; // Name of a section within your format
+?>
---
+++ course/format/onetopic/lang/es_utf8/format_onetopic.php
@@ -0,0 +1,4 @@
+<?php
+$string['formatonetopic'] = 'Temas en pestañas'; // Name to display for format
+$string['nameonetopic'] = 'tema'; // Name of a section within your format
+?>
