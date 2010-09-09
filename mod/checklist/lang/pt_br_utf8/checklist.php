6119347a3b072489896f482c5405f07371c5ca82
Ewout ter Haar <ewout@usp.br>
2010-09-09 16:34:06 -0300

    Tradução (do André ) do Checklist

---
+++ mod/checklist/lang/pt_br_utf8/checklist.php
@@ -0,0 +1,76 @@
+<?PHP // $Id$ 
+      // checklist.php - created with Moodle 1.9.9+ (Build: 20100907) (2007101590)
+      // local modifications from http://200.144.190.48/generic/cursos
+
+
+$string['addcomments'] = 'Adicionar comentários';
+$string['additem'] = 'Adicionar';
+$string['additemalt'] = 'Adicionar um novo item à lista';
+$string['additemhere'] = 'Inserir novo item depois deste';
+$string['addownitems'] = 'Adicionar seus próprios itens';
+$string['addownitems-stop'] = 'Parar de adicionar seus itens';
+$string['calendardescription'] = 'Este evento foi adicionado pelo checklist: $a';
+$string['canceledititem'] = 'Cancelar';
+$string['checkeditemsdeleted'] = 'Item selecionado deletado';
+$string['checklist'] = 'checklist';
+$string['checklist:edit'] = 'Criar e editar checklists';
+$string['checklist:preview'] = 'Visualizar checklist';
+$string['checklist:updateother'] = 'Atualizar checklists dos estudantes';
+$string['checklist:updateown'] = 'Atualizar seus checklists';
+$string['checklist:viewreports'] = 'Ver progresso dos estudantes';
+$string['checklistfor'] = 'Checklist para';
+$string['checklistintro'] = 'Introdução';
+$string['checklistsettings'] = 'Confiurações';
+$string['checks'] = 'Checar';
+$string['comments'] = 'Comentários';
+$string['confirmdeleteitem'] = 'Tem certeza que você quer apagar permanentemente este item do checklist?';
+$string['deleteitem'] = 'Apagar este item';
+$string['duedatesoncalendar'] = 'Adicionar datas de vencimento ao calendário';
+$string['edit'] = 'Editar checklist';
+$string['editdatesstart'] = 'Editar datas';
+$string['editdatesstop'] = 'Parar de editar datas';
+$string['edititem'] = 'Editar este item';
+$string['guestsno'] = 'Você não tem permissão para ver este checklist';
+$string['indentitem'] = 'Identar item';
+$string['itemcomplete'] = 'Completo';
+$string['items'] = 'Itens do checklist';
+$string['modulename'] = 'Checklist';
+$string['modulenameplural'] = 'Checklists';
+$string['moveitemdown'] = 'Mover item para baixo';
+$string['moveitemup'] = 'Mover item para cima';
+$string['noitems'] = 'Não há itens neste checklist';
+$string['optionalhide'] = 'Ocultar itens opcionais';
+$string['optionalitem'] = 'Este item é opcional';
+$string['optionalshow'] = 'Mostrar itens opcionais';
+$string['percentcomplete'] = 'Itens obrigatórios';
+$string['percentcompleteall'] = 'Todos os itens';
+$string['preview'] = 'Visualizar';
+$string['progress'] = 'Progresso';
+$string['report'] = 'Ver progresso';
+$string['reporttablesummary'] = 'Quadro com os itens de checklist que cada aluno tenha concluído';
+$string['requireditem'] = 'Este item é obrigatório e tem que ser completado';
+$string['resetchecklistprogress'] = 'Restaurar progresso do checklist e itens do usuário';
+$string['savechecks'] = 'Salvar';
+$string['showfulldetails'] = 'Exibir todos os detalhes';
+$string['showprogressbars'] = 'Mostrar barra de progresso';
+$string['teacheralongsidecheck'] = 'Aluno e professor';
+$string['teachercomments'] = 'Professor pode adicionar comentários';
+$string['teacheredit'] = 'Atualizações por';
+$string['teachermarkno'] = 'Segundo o tutor você ainda não completou esse item';
+$string['teachermarkundecided'] = 'O tutor não marcou esse item';
+$string['teachermarkyes'] = 'Segundo o tutor você já cumpriu esse item';
+$string['teachernoteditcheck'] = 'Somente aluno';
+$string['teacheroverwritecheck'] = 'Somente professor';
+$string['theme'] = 'Tema do checklist';
+$string['unindentitem'] = 'Desindentar item';
+$string['updateitem'] = 'Atualizar';
+$string['useritemsallowed'] = 'Usuários podem adicionar seus próprios itens';
+$string['useritemsdeleted'] = 'Itens do usuário apagados';
+$string['view'] = 'Ver checklist';
+$string['viewall'] = 'Ver o progresso de todos';
+$string['viewallcancel'] = 'Cancelar';
+$string['viewallsave'] = 'Salvar';
+$string['viewsinglereport'] = 'Ver o progresso desse aluno';
+$string['viewsingleupdate'] = 'Atualizar o progresso desse aluno';
+
+?>
